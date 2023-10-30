8a774a01194a23a0df3c76572ad5f66557497050
roal518 <roalqwerty@gmail.com>
2023-10-30 20:11:08 +0900

    unuse file delete

--- slicepoly_project/file/Mouse_vertex.glsl
+++
@@ -1,9 +0,0 @@
-# version 330 core
-
-layout (location = 2) in vec3 in_Mouse_Position;
-
-uniform mat4 modelMatrix;
-void main (void){
-
-	gl_Position =  modelMatrix * vec4(in_Mouse_Position,1.0);
-}
